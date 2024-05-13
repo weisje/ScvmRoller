a43b0a6735a242e6466302273356828e63fc7a64
weisje <20269574+weisje@users.noreply.github.com>
2024-05-13 14:15:38 -0400

    Added imports

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import os
+import random
+import sys
+
 def main():
     """
     The purpose of this program is to randomly generate characters for the TTRPG game "Mörk Borg" by Free League Publishing.  The characters are not from the optional classes; they are completely randomly generated based on the initial character rules.
