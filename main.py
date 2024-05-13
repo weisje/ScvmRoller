d1ccb7124a76c7630420009c66e26d8616ea27f9
weisje <20269574+weisje@users.noreply.github.com>
2024-05-13 13:36:19 -0400

    Added comment explaining why the code was written

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 def main():
+    """
+    The purpose of this program is to randomly generate characters for the TTRPG game "Mörk Borg" by Free League Publishing.  The characters are not from the optional classes; they are completely randomly generated based on the initial character rules.
+    """
     print("Success")
+
 
 if __name__ == 'main':
     main()
