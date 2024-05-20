6ab52076d53233c43a052e96f143dc8abf4c0fe8
weisje <20269574+weisje@users.noreply.github.com>
2024-05-20 07:08:00 -0400

    Populated characterNamesTable list in main() from characterNames.csv. Populated characterName by randomly selecting from characterNamesTable.  Tested functionality.

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ def main():
     """
     The purpose of this program is to randomly generate characters for the TTRPG game "Mörk Borg" by Free League Publishing.  The characters are not from the optional classes; they are completely randomly generated based on the initial character rules.
     """
-    print(traitPicker("TROUBLING TALES"))
+    characterNamesTable = csvReader("documents\\traits\\characterNames.csv", "|", False)
+    characterName = random.choice(characterNamesTable)
+    characterName = characterName[0]
+    print(characterName)
 
 
 def traitPicker(traitType) -> list:
