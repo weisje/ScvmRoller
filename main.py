ce85d8e708acfe376937293505867a946547607c
weisje <20269574+weisje@users.noreply.github.com>
2024-05-20 07:17:32 -0400

    Generated character stats with statsRoller(). Initialized hitPoints value to 1. Generated startingEquipment via equipmentRoller().  Provided checks for if scrolls are present.

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ def main():
     """
     The purpose of this program is to randomly generate characters for the TTRPG game "Mörk Borg" by Free League Publishing.  The characters are not from the optional classes; they are completely randomly generated based on the initial character rules.
     """
+    charStats = statsRoller()
+    hitPoints = 1
+    startingEquipment = equipmentRoller()
+    os.system('cls')
+    if startingEquipment[1][0] == "Unclean Scroll":
+        print("SCROLL 01")
+    if startingEquipment[2][0] == "Sacred Scroll":
+        print("SCROLL 02")
 
 
 def namePicker() -> str:
